46703450966a38e520c32bd61d30fc7292f3291a
David Castner <davidjcastner@gmail.com>
2016-04-30 22:28:45 -0400

    update gulp tasks with jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 
-gulp.task('test', function () {
+gulp.task('jshint', function() {
+    return gulp.src(['./gulpfile.js', './test/*.js', './lib/*.js'])
+    .pipe(jshint({ 'node': true, 'strict': true }))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('test', ['jshint'], function () {
     require('./test/test').runTests();
 });
 
 gulp.task('watch', ['test'], function () {
-    var watcher = gulp.watch('lib/*.*', ['test']);
+    var watcher = gulp.watch(['./gulpfile.js', './test/*', './lib/*'], ['test']);
     watcher.on('change', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 });
 
-gulp.task('travis', function () {
+gulp.task('travis', ['jshint'], function () {
     var pass = require('./test/test').runTests();
     if (!pass) {
         throw "Tests failed!";
