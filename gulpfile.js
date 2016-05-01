e93672156fa05b4ebf2cf78e85a3ce099c1fff48
David Castner <davidjcastner@gmail.com>
2016-05-01 14:23:20 -0400

    add debug tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
+var fs = require('fs');
 var reload = require('require-reload')(require);
 var jshint = require('gulp-jshint');
+
 
 gulp.task('jshint', function() {
     return gulp.src(['./gulpfile.js', './test/*.js', './lib/*.js'])
@@ -35,12 +37,67 @@ gulp.task('travis', ['jshint'], function () {
 
 gulp.task('default', ['watch']);
 
-gulp.task('generateSyntaxExample', function () {
-    var readFileSync = require('fs').readFileSync;
-    var wrapperHead = readFileSync('./test/example/wrapperHead.html', 'utf8');
-    var wrapperFoot = readFileSync('./test/example/wrapperFoot.html', 'utf8');
+gulp.task('debugScanner', function () {
     var inputFile = './test/example/example.thyme';
-    var source = readFileSync(inputFile, 'utf8');
+    var source = fs.readFileSync(inputFile, 'utf8');
+    var Scanner = reload('./lib/Scanner').Scanner;
+    var scanner = new Scanner(inputFile, source);
+    var character = scanner.next();
+    while (character) {
+        console.log(character.toString());
+        character = scanner.next();
+    }
+});
+
+gulp.task('scanner', ['debugScanner'], function () {
+    var watcher = gulp.watch(['./test/*.*', './lib/*.*'], ['debugScanner']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('debugLexer', function () {
+    var inputFile = './test/example/example.thyme';
+    var source = fs.readFileSync(inputFile, 'utf8');
+    var tokenTypes = reload('./lib/thymeTokens').tokens;
+    var Lexer = reload('./lib/Lexer').Lexer;
+    var lexer = new Lexer(inputFile, source, tokenTypes);
+    var token = lexer.next();
+    while (token) {
+        console.log(token.toString());
+        token = lexer.next();
+    }
+});
+
+gulp.task('lexer', ['debugLexer'], function () {
+    var watcher = gulp.watch(['./test/*.*', './lib/*.*'], ['debugLexer']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('debugParser', function () {
+    var inputFile = './test/example/example.thyme';
+    var source = fs.readFileSync(inputFile, 'utf8');
+    var tokenTypes = reload('./lib/thymeTokens').tokens;
+    var Parser = reload('./lib/Parser').Parser;
+    var parser = new Parser({ indentMode: true }, inputFile, source, tokenTypes);
+    var parseTree = parser.parse();
+    console.log(parseTree.toString());
+});
+
+gulp.task('parser', ['debugParser'], function () {
+    var watcher = gulp.watch(['./test/*.*', './lib/*.*'], ['debugParser']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('generateSyntaxExample', function () {
+    var wrapperHead = fs.readFileSync('./test/example/wrapperHead.html', 'utf8');
+    var wrapperFoot = fs.readFileSync('./test/example/wrapperFoot.html', 'utf8');
+    var inputFile = './test/example/example.thyme';
+    var source = fs.readFileSync(inputFile, 'utf8');
     var tokenTypes = require('./lib/thymeTokens').tokens;
     var Lexer = require('./lib/Lexer').Lexer;
     var lexer = new Lexer(inputFile, source, tokenTypes);
@@ -53,5 +110,5 @@ gulp.task('generateSyntaxExample', function () {
     var writeFileSync = require('fs').writeFileSync;
     var completeFile = wrapperHead + output + wrapperFoot;
     var outputFile = './test/example/syntaxHighlight.html';
-    writeFileSync(outputFile, completeFile);
+    fs.writeFileSync(outputFile, completeFile);
 });
